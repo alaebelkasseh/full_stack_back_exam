7c328434463b80ff0a9cb0ea718e6ea49e7b04bc
BELKASSEH <rabie.belkasseh@capgemini.com>
2023-10-22 16:10:22 +0200

    creation view

--- reservation_hotel/app_de_reservation_hotel/views.py
+++ reservation_hotel/app_de_reservation_hotel/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Reservation
+from .models import Chambre
+from .serializers import ReservationSerializer
+from .serializers import ChambreSerializer
+
+class ReservationViewSet(viewsets.ModelViewSet):
+
+    queryset = Reservation.objects.all()
+
+    serializer_class = ReservationSerializer
+
+class ChambreViewSet(viewsets.ModelViewSet):
+
+    queryset = Reservation.objects.all()
+
+    serializer_class = ChambreSerializer
