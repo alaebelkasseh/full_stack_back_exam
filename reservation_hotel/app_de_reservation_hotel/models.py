0e7b5a023be7e2db9a2e3a7e3f27e664fe8b0ab1
BELKASSEH <rabie.belkasseh@capgemini.com>
2023-10-22 16:10:02 +0200

    creation modeles

--- reservation_hotel/app_de_reservation_hotel/models.py
+++ reservation_hotel/app_de_reservation_hotel/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Reservation(models.Model):
+
+    nom = models.CharField(max_length=100)
+
+    date_d_arrivee = models.DateField()
+
+    date_de_depart = models.DateField()
+
+    chambre = models.CharField(max_length=10)
+    
+class Chambre(models.Model):
+
+    numero = models.CharField(max_length=10)
+
+    type = models.CharField(max_length=100)
+
+
+
